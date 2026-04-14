f06d40421e3cb64b2a742064ea52c6b5c374fea3
My Linh Lu <mylinhxlu@gmail.com>
2026-04-14 16:18:11 +0200

    Basic login page

---
+++ src/main/java/nl/miwnn/ch19/DaMaGe/IntraClass/config/IntraClassSecurityConfig.java
@@ -0,0 +1,62 @@
+package nl.miwnn.ch19.DaMaGe.IntraClass.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * @author My Linh Lu
+ */
+@Configuration
+@EnableWebSecurity
+public class IntraClassSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/",
+                                "/person"
+                        ).permitAll()
+                        .requestMatchers(
+                                "/person-form"
+                        ).hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/person")
+                        .permitAll()
+                )
+                .formLogin(form -> form
+                        .loginPage("/")
+                        .defaultSuccessUrl("/person", true)
+                        .permitAll()
+                );
+        return http.build();
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(
+            PasswordEncoder encoder) {
+        var beheerder = User.builder()
+                .username("admin")
+                .password(encoder.encode("geheim123"))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(beheerder);
+    }
+}
--- src/main/java/nl/miwnn/ch19/DaMaGe/IntraClass/controller/InitializeController.java
+++ src/main/java/nl/miwnn/ch19/DaMaGe/IntraClass/controller/InitializeController.java
@@ -2,11 +2,15 @@ package nl.miwnn.ch19.DaMaGe.IntraClass.controller;
 
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
+import nl.miwnn.ch19.DaMaGe.IntraClass.model.IntraClassUser;
 import nl.miwnn.ch19.DaMaGe.IntraClass.model.Person;
 import nl.miwnn.ch19.DaMaGe.IntraClass.repository.PersonRepository;
+import nl.miwnn.ch19.DaMaGe.IntraClass.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 
 import java.io.IOException;
@@ -22,16 +26,33 @@ import java.io.Reader;
 public class InitializeController {
 
     private final PersonRepository personRepository;
+    @Autowired
+    private final UserRepository userRepository;
 
+    @Autowired
+    private final PasswordEncoder passwordEncoder;
 
-    public InitializeController(PersonRepository personRepository) {
+    public InitializeController(
+            PersonRepository personRepository,
+            UserRepository userRepository,
+            PasswordEncoder passwordEncoder) {
         this.personRepository = personRepository;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @EventListener(ContextRefreshedEvent.class)
     public void seed() {
         if(personRepository.count() == 0) {
             seedPeople();
+        }
+        if (userRepository.count() == 0) {
+            IntraClassUser admin = new IntraClassUser(
+                    "admin",
+                    passwordEncoder.encode("geheim123"),
+                    "ADMIN"
+            );
+            userRepository.save(admin);
         }
     }
 
