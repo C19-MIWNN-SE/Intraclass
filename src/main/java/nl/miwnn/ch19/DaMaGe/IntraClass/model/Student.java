2067436df29d2e1b4818ce3ad56225fc1eea0602
Fullestcommerce <dudardanilo@gmail.com>
2026-04-14 16:01:40 +0200

    added getters and setter from Lombok

--- src/main/java/nl/miwnn/ch19/DaMaGe/IntraClass/model/Student.java
+++ src/main/java/nl/miwnn/ch19/DaMaGe/IntraClass/model/Student.java
@@ -2,6 +2,8 @@ package nl.miwnn.ch19.DaMaGe.IntraClass.model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,19 +18,25 @@ import java.util.List;
 public class Student{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter @Setter
     private Long id;
 
     @NotBlank(message = "Bio can't be blank")
+    @Getter @Setter
     private String bio;
 
+    @Getter @Setter
     private String employer;
 
     @NotBlank(message = "City can't be blank")
+    @Getter @Setter
     private String city;
 
     @OneToOne(mappedBy = "student", cascade = CascadeType.ALL)
+    @Getter @Setter
     private Person person;
 
     @ManyToMany(mappedBy = "student", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @Getter @Setter
     private List<Cohort> cohort = new ArrayList<>();
 }
--- src/main/java/nl/miwnn/ch19/DaMaGe/IntraClass/model/Teacher.java
+++ src/main/java/nl/miwnn/ch19/DaMaGe/IntraClass/model/Teacher.java
@@ -2,6 +2,8 @@ package nl.miwnn.ch19.DaMaGe.IntraClass.model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,14 +19,18 @@ import java.util.List;
 public class Teacher{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter @Setter
     private Long id;
 
     @NotBlank(message = "Teacher specialty cannot be blank")
+    @Getter @Setter
     private String specialty;
 
     @OneToOne(mappedBy = "teacher", cascade = CascadeType.ALL)
+    @Getter @Setter
     private Person person;
 
     @ManyToMany(mappedBy = "teacher", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @Getter @Setter
     private List<Cohort> cohort = new ArrayList<>();
 }
