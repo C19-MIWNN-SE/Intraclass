b3706f24c14d2cfba18b660851cca98634825d07
My Linh Lu <mylinhxlu@gmail.com>
2026-04-14 15:58:17 +0200

    Basic login page with hard coded user

---
+++ src/main/java/nl/miwnn/ch19/DaMaGe/IntraClass/config/IntraClassSecurityConfig.java
@@ -0,0 +1,7 @@
+package nl.miwnn.ch19.DaMaGe.IntraClass.config;
+
+/**
+ * @author My Linh Lu
+ * !! BESCHRIJVING !!
+ */public class IntraClassSecurityConfig {
+}
---
+++ src/main/java/nl/miwnn/ch19/DaMaGe/IntraClass/controller/IndexController.java
@@ -0,0 +1,7 @@
+package nl.miwnn.ch19.DaMaGe.IntraClass.controller;
+
+/**
+ * @author My Linh Lu
+ * !! BESCHRIJVING !!
+ */public class IndexController {
+}
