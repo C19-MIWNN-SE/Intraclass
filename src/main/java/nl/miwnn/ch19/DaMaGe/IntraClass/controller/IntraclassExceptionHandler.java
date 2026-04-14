f814fec9cf7d74dce9880f674a9ed343bcc0fac5
unknown <dudardanilo@gmail.com>
2026-04-14 09:30:58 +0200

    Implemented exception handler, created a fragment for person table

---
+++ src/main/java/nl/miwnn/ch19/DaMaGe/IntraClass/controller/IntraclassExceptionHandler.java
@@ -0,0 +1,31 @@
+package nl.miwnn.ch19.DaMaGe.IntraClass.controller;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * @author Danylo Dudar
+ *Does what it's name says
+ */
+
+@ControllerAdvice
+public class IntraclassExceptionHandler {
+    private static final Logger log = LoggerFactory.getLogger(IntraclassExceptionHandler.class);
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleNotFound(
+            EntityNotFoundException entityNotFoundException,
+            Model model
+    ) {
+        log.warn("Entity not found, exception message: {}", entityNotFoundException.getMessage());
+        model.addAttribute("message", entityNotFoundException.getMessage());
+        return "error/404";
+    }
+}
