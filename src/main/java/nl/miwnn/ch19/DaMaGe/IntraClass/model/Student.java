b066e50959a514567dbd10de05211c79d67db8fd
Fullestcommerce <dudardanilo@gmail.com>
2026-04-15 15:07:09 +0200

    Modified student and teacher as extention of person. removed XOR validation. added automatic csv data injection

--- src/main/java/nl/miwnn/ch19/DaMaGe/IntraClass/model/Student.java
+++ src/main/java/nl/miwnn/ch19/DaMaGe/IntraClass/model/Student.java
@@ -1,5 +1,6 @@
 package nl.miwnn.ch19.DaMaGe.IntraClass.model;
 
+import com.opencsv.bean.CsvBindByName;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
@@ -10,16 +11,10 @@ import java.util.List;
 
 /**
  * @author Danylo Dudar
- *Extends Person as a Student(trust me)
- * Check validation folder for more info
+ *
  */
 @Entity
-@Table
-public class Student{
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
+public class Student extends Person {
     @NotBlank(message = "Bio can't be blank")
     private String bio;
 
@@ -28,19 +23,8 @@ public class Student{
     @NotBlank(message = "City can't be blank")
     private String city;
 
-    @OneToOne(mappedBy = "student", cascade = CascadeType.ALL)
-    private Person person;
-
     @ManyToMany(mappedBy = "student", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     private List<Cohort> cohort = new ArrayList<>();
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public String getBio() {
         return bio;
@@ -64,14 +48,6 @@ public class Student{
 
     public void setCity(String city) {
         this.city = city;
-    }
-
-    public Person getPerson() {
-        return person;
-    }
-
-    public void setPerson(Person person) {
-        this.person = person;
     }
 
     public List<Cohort> getCohort() {
